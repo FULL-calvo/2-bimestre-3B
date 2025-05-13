90335cea29be6365502b74760d58735d8b31a201
Leandro <leandrodemoraesgoncalves@gmail.com>
2025-05-13 13:41:41 -0300

    save 3.5

--- site-react/src/Routes/AppRoutes.jsx
+++ site-react/src/Routes/AppRoutes.jsx
@@ -1,16 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Details from "./pages/Details";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter, Link } from "react-router-dom";
+import Home from "../pages/home";
+import Favoritos from "../pages/favoritos";
+import Detalhes from "../pages/detalhes";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/details/:id" element={<Details />} />
-      </Routes>
-    </Router>
+export default function AppRoutes() {
+  return(
+      <>
+          <BrowserRouter>
+            <nav>
+              <Link to="/">home</Link>
+              <Link to="/detalhes">detalhes</Link>
+              <Link to="/fovoritos">favoritos</Link>
+            </nav>
+
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/detalhes/:id" element={<Detalhes />} />
+              <Route path="/favoritos" element={<Favoritos />} />
+            </Routes>
+          </BrowserRouter>
+      </>
   );
 }
-
-export default App;
