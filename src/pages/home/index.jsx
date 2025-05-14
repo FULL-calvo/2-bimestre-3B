aa4f6d96ff191ad5e72ea91d2d455c506f50bbab
Leandro <leandrodemoraesgoncalves@gmail.com>
2025-05-14 09:52:22 -0300

    correção npm

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts?_limit=10") 
+      .then((res) => (setPosts(res.data), console.log(res.data)));
+      
+  }, []);
+
+  return (
+    <>
+    <div>
+      <h2>Lista de posts</h2>
+      <ul>
+            {posts.map((posts) => (
+              <li key={posts.id}>
+                <Link to= {`/detalhes/${posts.id}`}>{posts.title}</Link>
+              </li>
+          ))}
+      </ul>
+    </div>
+    </>
+  )
+} 
